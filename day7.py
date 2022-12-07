0a6beb05ce34786fbb701acc274c6fc3fbc8fbf3
Eric <eric-noren@hotmail.com>
2022-12-07 15:48:36 +0100

    Finish day7 yaaay!

--- day7.py
+++ day7.py
@@ -2,29 +2,26 @@ import parser_util
 
 inp = parser_util.readList("inputs/7.txt", "string", True, False)
 
-
 def build_tree():
-
-    inp.pop(0)  # Remove initial cd /
-    #prevDir = ''
     prevDirStack = []
     currDir = '/'
-    tree = {'/': []}
     sizeMap = {'/': 0}
-
+    
     for l in inp:
-        if " ls" in l or "dir" in l:
+        fil = l.split(" ")
+        if "$ ls" in l or "dir" in l:
             continue
-        elif "cd " in l:
+        elif "$ cd" in l:
             cmd = l.split(" ")
             newDir = cmd[2]
 
             if ".." in newDir:
                 currDir = prevDirStack.pop()
             elif newDir == '/':
+                prevDirStack = []
                 currDir = '/'
             else:
-                while newDir in sizeMap:
+                while newDir in sizeMap.keys():
                     newDir = newDir+'x'
 
                 prevDirStack.append(currDir)
@@ -32,46 +29,27 @@ def build_tree():
 
                 sizeMap.update({currDir: 0})
         else:
-            file = l.split(" ")
-            sizeMap[currDir] = sizeMap.get(currDir) + int(file[0])
+            sizeMap[currDir] = sizeMap.get(currDir) + int(fil[0])
 
             for pDir in prevDirStack:
-                sizeMap[pDir] = sizeMap.get(pDir) + int(file[0])
+                sizeMap[pDir] += int(fil[0])
 
     result = 0
-    for dir in sizeMap.keys():
-        if sizeMap.get(dir) <= 100000:
-            result += sizeMap.get(dir)
-    print("Dirs with size <= 100000")
-    print(result)
+    for d in sizeMap.keys():
+        if sizeMap.get(d) <= 100000:
+            result += sizeMap.get(d)
+    print("Dirs with size <= 100000: ", result)
 
     maxSpace = 70000000
     needed = 30000000
     unusedSpace = maxSpace - sizeMap.get('/')
-    toDelete = needed - unusedSpace
-    print("UNUSED")
-    print(unusedSpace)
-    print("TOL DELETE")
-    print(toDelete)
 
     dSizes = []
-    for dir in sizeMap.keys():
-        dSizes.append(sizeMap.get(dir))
+    for d in sizeMap.keys():
+        dSizes.append(sizeMap.get(d))
+
+    dSizes = [d for d in dSizes if d+unusedSpace > needed]
     dSizes.sort()
-    print(dSizes)
-
-    smallestDiff = maxSpace
-    smallest = maxSpace
-    for d in dSizes:
-        if d >= toDelete:
-            diff = d - toDelete
-            if diff < smallestDiff:
-                smallestDiff = diff
-                smallest = d
-                print(d)
-
-    print("Smallest to delete:")
-    print(smallest)
-
+    print("Delete folder with size: ", min(dSizes))
 
 build_tree()
